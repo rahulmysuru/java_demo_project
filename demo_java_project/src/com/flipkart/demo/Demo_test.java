903d871bdbe984ecb91d56c48eb717246dbc8884
Rahul S <rahulmysuru7@gmail.com>
2023-06-19 15:43:44 +0530

    First java commit

---
+++ demo_java_project/src/com/flipkart/demo/Demo_test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class Demo_test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first app");
+
+	}
+
+}
